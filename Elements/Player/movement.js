30c912f7c6d116dc762837b8c69d5b72ebd39543
Rhylie <rhylie.williams@gmail.com>
2021-03-15 12:18:15 -0400

    i have to do this

--- Elements/Player/movement.js
+++ Elements/Player/movement.js
@@ -1,5 +1,9 @@
-document.addEventListener("keydown", makeJump);//listens for a key press
-function makeJump(e) {
+document.addEventListener("keydown", makeBounce);//listens for a key press
+function makeBounce(e) {
  if (e.key == " ") {//if the spacebar is pressed the ball gains y velcity
    dy -= 3;
  }
+ if (e.key == "r") {//if thr "r" key is pressed the x direction is flipped
+   gameState == 1;
+ }
+}
