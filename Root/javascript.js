f13ba4e2e4274cbbe343ae3139e95a3865f3e768
Fly2Freedom <evanninja3@gmail.com>
2021-03-15 11:32:59 -0400

    made canvas look fancy

--- Root/javascript.js
+++ Root/javascript.js
@@ -1,1 +1,7 @@
+var canvas = document.getElementById("game-canvas");
+var ctx = canvas.getContext("2d");
 
+window.onload = function() {
+  ctx.fillStyle = "white";
+  ctx.fillRect(0,0,1280,720);
+}
