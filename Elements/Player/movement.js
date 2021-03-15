2fab18f4964ceb4bd7a921eda945359e697b28c4
andrew horner <analeigh221@gmail.com>
2021-03-15 12:22:57 -0400

    we might have un jump

--- Elements/Player/movement.js
+++ Elements/Player/movement.js
@@ -1,9 +1,5 @@
-document.addEventListener("keydown", makeBounce);//listens for a key press
-function makeBounce(e) {
+document.addEventListener("keydown", makeJump);//listens for a key press
+function makeJump(e) {
  if (e.key == " ") {//if the spacebar is pressed the ball gains y velcity
    dy -= 3;
  }
- if (e.key == "r") {//if thr "r" key is pressed the x direction is flipped
-   gameState == 1;
- }
-}
