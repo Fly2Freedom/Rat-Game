7b6ea9559eb74c7b245cbcfca0df3fc20468d6e3
andrew horner <analeigh221@gmail.com>
2021-03-16 12:24:03 -0400

    finished the movement and we have a class too in amogus

---
+++ Root/Elements/Enemies/amogus/spawn.js
@@ -0,0 +1,11 @@
+class Amogus {
+  constructor(name, color) {
+    this.name = name;
+    this.color  = color;
+  }
+  age() {
+    let position = new x.position += y.position;
+    return movement.getFinalPosition() - this.startingPosition;
+  }
+  console.log();
+}
