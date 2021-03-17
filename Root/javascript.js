6dc49272aedb1a4e17784ca7c9d3fdd550edcf44
Fly2Freedom <evanninja3@gmail.com>
2021-03-17 09:24:00 -0400

    you can now move a box!

--- Root/javascript.js
+++ Root/javascript.js
@@ -1,3 +1,59 @@
+class Player {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.xspeed = 0;
+    this.yspeed = 0;
+    this.friction = 0.6;
+    this.gravity = 0.8
+    this.maxSpeed = 5;
+    this.width = 50;
+    this.height = 100;
+    this.active = true;
+    this.jump = false;
+  }
+
+  spawn() {
+    ctx.beginPath()
+    ctx.fillStyle = "#000000";
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+    ctx.stroke();
+  }
+
+  move() {
+    if (this.active) {
+      if (!keyLeft && !keyRight || keyLeft && keyRight) {
+        this.xspeed *= this.friction;
+      } else if (keyRight) {
+        this.xspeed = 5;
+      } else if (keyLeft) {
+        this.xspeed = -5;
+      }
+      //Jump
+      if (keyUp) {
+        this.yspeed -= 1.5;
+      }
+      this.yspeed += this.gravity;
+
+      //Cap speed at a certain amount (might not even need?)
+      if (this.xspeed > this.maxSpeed) {
+        this.xspeed = this.maxSpeed;
+      } else if (this.xspeed < -this.maxSpeed) {
+        this.xspeed = -this.maxSpeed;
+      }
+      if (this.yspeed > this.maxSpeed) {
+        this.yspeed = this.maxSpeed;
+      } else if (this.xspeed < -this.maxSpeed) {
+        this.yspeed = -this.maxSpeed;
+      }
+
+      //Update the player's coordinates.
+      this.x += this.xspeed;
+      this.y += this.yspeed;
+    }
+  }
+}
+
 var canvas = document.getElementById("game-canvas");
 var ctx = canvas.getContext("2d");
 
@@ -9,18 +65,17 @@ var keyDown;
 var keyLeft;
 var keyRight;
 
+var player = new Player(600, 618);
+
 window.onload = function() {
   //Set up key inputs
   inputs();
-  //Create new player
-  const player = new Player(600, 618);
   //Start drawing loop
-  gameLoop = setInterval(move, 1000/300);
+  gameLoop = setInterval(move, 1000/60);
 }
 
 function move() {
-  player.move()
-
+  player.move();
   clear();
 }
 
